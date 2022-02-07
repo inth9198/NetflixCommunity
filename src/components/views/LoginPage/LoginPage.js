92e7db884107cd3a83bf510622d057b26d65d1ec
minholee <inth2474@naver.com>
2022-02-08 00:05:24 +0900

    login

---
+++ src/components/views/LoginPage/LoginPage.js
@@ -0,0 +1,42 @@
+import { Axios } from "axios";
+import React, { useState } from "react";
+
+function LoginPage(){
+    const [Email, setEmail] = useState("");
+    const [Password, setPassword] = useState("")
+    const onEmailHandler = (event) =>{
+        setEmail(event.target.value);
+    }
+    const onPasswordHandler = (event) =>{
+        setPassword(event.target.value);
+    }
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        let body = {
+            email : Email,
+            password : Password
+        }
+        //Axios.post('')서버에 보내기
+    }
+    return(
+        <div style={{
+            display:'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height:'100vh'
+        }}>
+            <form 
+                style={{ display: 'flex', flexDirection: 'column' }}
+                onSubmit={onSubmitHandler}
+            >
+            <label>Email</label>
+				<input type="email" value={Email} onChange={onEmailHandler} />
+				<label>Password</label>
+				<input type="password"  value={Password} onChange={onPasswordHandler} />
+				<br />
+				<button type="submit">
+					Login
+				</button>
+            </form>
+        </div>
+    )
+}
+export default LoginPage
