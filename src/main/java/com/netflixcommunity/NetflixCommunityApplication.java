f39e14480040fb39b2203fdb3101bca0349b6432
smileyr909 <smileyr909@gmail.com>
2022-02-14 18:25:42 +0900

    initial commit

---
+++ src/main/java/com/netflixcommunity/NetflixCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.netflixcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixCommunityApplication.class, args);
+    }
+
+}
